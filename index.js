99c74328f8578c8b15b5cd8b1dfce1fca964275e
lucyq <lucyzqin@gmail.com>
2017-11-09 16:22:50 -0500

    traversing

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const util = require('util');
 const babylon = require('babylon');
 
-const code = `
-    var x = 5;
-`;
+const code = `function f(a,b) {return a + b;}`;
 
 
 var AST = babylon.parse(code);
 
+var AST = AST.program.body[0];
 
-console.log(util.inspect(AST, false, null));
+traverseAST(AST);
+
+
+function handleChildren(AST) {
+
+  if (AST.argument.type === "BinaryExpression") {
+    // console.log(AST.argument) 
+    console.log(AST.argument.operator)
+  }
+
+  // console.log(AST.argument.type);
+  // if (AST.argument.type)
+  // for (k in AST.argument) {
+    
+  // }
+
+
+}
+
+function traverseAST(AST) {
+  if (AST.type === 'FunctionDeclaration') {
+    // for (k in AST.body) {
+    if (AST.body.body != undefined) {
+      var body = AST.body.body;
+
+      for (var i = 0; i < body.length; i++) {
+        handleChildren(body[i]);
+      }
+    }
+  }
+}
