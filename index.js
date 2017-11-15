a1e62e9002166661b39114c99264d05a34f1ffda
lucyq <lucyzqin@gmail.com>
2017-11-15 15:37:34 -0500

    mustaching

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const util = require('util');
 const babylon = require('babylon');
+const traverse = require('babel-traverse');
 
-const code = `function f(a,b) {return a + b;}`;
+const code = `function f(a,b) {return a + b + c + d;}`;
 
 
 var AST = babylon.parse(code);
 
 var AST = AST.program.body[0];
 
+const ADD = '+';
+const SUB = '-';
+const MULT = "*";
+const DIV = "/";
+
+
 traverseAST(AST);
 
-
-function handleChildren(AST) {
-
-  if (AST.argument.type === "BinaryExpression") {
-    // console.log(AST.argument) 
-    console.log(AST.argument.operator)
-  }
-
-  // console.log(AST.argument.type);
-  // if (AST.argument.type)
-  // for (k in AST.argument) {
+function translateOperator(op) {
+  if (op === ADD) {
     
-  // }
+  } else if (op === SUB) {
 
+  } else if (op === MULT) {
+
+  } else if (op === DIV) {
+
+  }
+}
+
+function translateToJiff(tree) {
+  var l = translateToJiff(tree.left);
+  var r = translateToJiff(tree.right);
+
+  translateOperator(tree.operator);
 
 }
 
@@ -34,7 +44,10 @@ function traverseAST(AST) {
       var body = AST.body.body;
 
       for (var i = 0; i < body.length; i++) {
-        handleChildren(body[i]);
+        if (body[i].argument.type === "BinaryExpression") {
+          translateToJiff(body[i].argument);
+
+        }
       }
     }
   }
