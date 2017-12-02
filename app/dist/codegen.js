14e881f20621a663f5b79ed412eb3ca4b863ac74
lucyq <lucyzqin@gmail.com>
2017-12-02 13:12:06 -0500

    saving before switching branches

--- app/dist/codegen.js
+++ app/dist/codegen.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// // translate arithmetic operators to jiff function names
+// translate arithmetic operators to jiff function names
 var op_translate = {
     '+': 'add',
     '-': 'sub',
@@ -22,12 +22,7 @@ module.exports = function (babel) {
         if (t.isIdentifier(left)) {
             var expr = t.callExpression(t.memberExpression(t.identifier(left.name), t.identifier(op)), [right]);
         }
-        // TODO: can't actually have an integer in the left-most position,
-        // since (i think) jiff doesn't support <constant>.<op>(variable) statements.
-        // should probably just return an error message here instead.
-        // TODO: flipping them would be easy, but this will have to be handled differently
-        // for division once it's implemented (7 / x != x.div(7))
-        // TODO: flipping would also be different for subtraction: 7 - a would be a + (-7)
+        // TODO: possibly change, can't have literal in left-most position
         else if (t.isNumericLiteral(left)) {
                 var expr = t.callExpression(t.memberExpression(t.numericLiteral(left.value), t.identifier(op)), [right]);
             } else if (t.isUnaryExpression(left)) {
@@ -45,22 +40,18 @@ module.exports = function (babel) {
         return expr;
     }
 
+    // TODO: test for '!' presence, add '.not'
     // traverse & transform nodes in a binary op
     function bin_rec_transform(path) {
+        // reached left-most value
         if (t.isIdentifier(path.node.left) || t.isNumericLiteral(path.node.left) || t.isUnaryExpression(path.node.left)) {
             if (path.node.operator in op_translate) {
-
-                // calculateCost(path.node.operator, path);
-
                 path.replaceWith(bin_leaf(path.node.left, path.node.right, op_translate[path.node.operator]));
-            } else if (eq_ops.has(path.node.operator)) {
-                // handle '===' and '!=' here
-                // can't do straight equality testing, so need share.<eq_test> (i think)
-                // TODO: ask kinan & rawane
+            } else {
+                console.log("Unknown binary operation.");
             }
         } else {
             bin_rec_transform(path.get('left'));
-            // calculateCost(path.node.operator, path);
             path.replaceWith(bin_nonleaf(path.node.left, path.node.right, op_translate[path.node.operator]));
         }
     }
@@ -69,7 +60,9 @@ module.exports = function (babel) {
     function tern_conditional(path) {
         // handle !<cond> ? <expr1> <expr2> case
         if (t.isUnaryExpression(path.node.test) && path.node.test.operator === '!') {
-            console.log("Hi i am here");
+            var left = t.binaryExpression('*', path.node.test, path.node.consequent);
+            var right = t.binaryExpression('*', path.node.test.argument, path.node.alternate);
+            path.replaceWith(t.binaryExpression('+', left, right));
         }
         // handle <cond> ? <expr1> <expr2> case
         else {
