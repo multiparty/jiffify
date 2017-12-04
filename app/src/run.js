2902dd3599078f8d8e59ef4c5e432dfce994c1cc
Ben Getchell <bengetch@gmail.com>
2017-12-04 15:49:55 -0500

    little more cleanup

--- app/src/run.js
+++ app/src/run.js
@@ -1,30 +1,16 @@
-import * as babylon from "babylon";
-import traverse from "babel-traverse";
-import * as t from "babel-types";
 const babel = require('babel-core');
 const jiffify = require('./jiffify');
 const analysis = require('./analysis');
 
 function parseCode(src) {
-
-    var converted = babel.transform(src, {
-        plugins: [jiffify]
-    });
-    var analyzed = babel.transform(converted.code, {
-        plugins: [analysis]
-    });
-
-    // return converted.code;
+    var converted = babel.transform(
+        src, { plugins: [jiffify] }
+        );
+    var analyzed = babel.transform(
+        converted.code, { plugins: [analysis] }
+        );
+    // return converted code;
     return {code: converted.code, costs: analyzed.ast.program.costObject};
-
 }
-
-/*
-var src = 'function fun(a,b,c) {var a = (b > c) ? b : c; return a;}'
-
-var new_src = parseCode(src);
-
-console.log(new_src);
-*/
 
 module.exports.parseCode = parseCode;
